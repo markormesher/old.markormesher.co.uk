c034d193221337d75a21ac4a59b463eca4df1b95
Mark Ormesher <me@markormesher.co.uk>
2014-10-09 14:07:37 +0100

    Basic view of blog posts

--- src/wp-content/themes/markormesher/blog-index/index.php
+++ src/wp-content/themes/markormesher/blog-index/index.php
@@ -1,22 +1,23 @@
 <div class="page">
 	<div class="container column-container">
 		<div class="eight column">
-			<h1>
-				<?php
-				if (is_tag()) {
-					single_tag_title('#');
-				} else {
-					wp_title('');
-				}
-				?>
-			</h1>
 			<?php
+			if (is_tag()) {
+				echo('<h1>#'. single_tag_title('', false) . '</h1>');
+			}
 			if (have_posts()) {
-				while (have_posts()){
+				$first = true;
+				while (have_posts()) {
 					the_post();
-			?>
-				<p><?=get_the_title();?></p>
-			<?php
+					if ($first) {
+						$first = false;
+					} else {
+						echo('<div class="hr"><hr/></div>');
+					}
+					echo('<h3><a href="' . get_the_permalink() . '" title="' . get_the_title() . '">' . get_the_title() . '</a></h3>');
+					echo('<p class="date-line">' . get_the_date() . '</p>');
+					echo(get_the_excerpt());
+					echo('<p><i class="fa fa-arrow-circle-right orange"></i> <a href="' . get_the_permalink() . '" title="' . get_the_title() . '">Read the Full Post</a></p>');
 				}
 			}
 			?>
