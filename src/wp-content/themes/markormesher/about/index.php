1b19afda29d9cf9783a245064234a5a8f06c079b
Mark Ormesher <me@markormesher.co.uk>
2014-10-10 00:54:03 +0100

    Initial about me page content

--- src/wp-content/themes/markormesher/about/index.php
+++ src/wp-content/themes/markormesher/about/index.php
@@ -1,13 +1,27 @@
 <div class="page">
 	<div class="container column-container">
-		<div class="eight column">
-			<p>Content</p>
+		<h1>About Me</h1>
+		<div class="five-half column">
+			<h2>Right Now: Work</h2>
+			<p>As the <strong>lead Android developer</strong> for <a href="http://www.unitu.co.uk" title="Unitu: Learn Better, Together" rel="external">Unitu</a>, I am responsible for building and testing an app that will soon be deployed to 2500 UK university students.</p>
+			<h2>Programming</h2>
+			<p>I have been programming for 9 years, as a hobby, a trade and a passion. I have <a href="/skills" title="Mark Ormesher's programming skill">skills</a> in <strong>mobile, desktop and web</strong> technologies, including Android, Java, Python, PHP, JS/jQuery, (X)HTML and CSS.</p>
+			<h2>Teaching</h2>
+			<p>As a <strong>teaching assistant</strong> at King's College, I provide support for the Advanced Practical Topics module, which gives students an insight to topics that they wouldn't otherwise cover.</p>
+			<p>In 2012/13, I also volunteered with the charity Project Trust as a <strong>secondary school teacher</strong> in South America. I taught maths, reading and PE to youths aged 13-16 in a remote hinterland village, and ran several extra-curricular projects.</p>
+			<p>My experiences overseas sparked a passion for education: since my return I have promoted Project Trust at careers fairs and involved myself in a number of educational projects, including a series of <strong>Android workshops</strong> that I organise and deliver through my work with the <a href="http://www.kcltech.com" title="KCL Tech Society" rel="external">KCL Tech Society</a>.</p>
 		</div>
 		<div class="one column column-spacer">
 			&nbsp; <!-- blank -->
 		</div>
-		<div class="three column">
-			<p>Content</p>
+		<div class="five-half column">
+			<h2>Right Now: Studies</h2>
+			<p>I am a second-year Computer Science student at <strong>King's College London</strong>. I maintained a 93% average over my first year, and in 2015/16 I will complete a year-long industry placement.</p>
+			<h2>Awards</h2>
+			<p>In July 2014 I was awarded the <strong>British Computing Society Award for Best Student</strong>, as well as three other departmental awards from <strong>KCL</strong>, including best first-year results and an award for exceptional contribution.</p>
+			<h2>Travel</h2>
+			<p>During my year of volunteering, I was lucky enough to travel to several South American and Caribbean countries, which I have <a href="http://www.marksprojecttrust.co.uk" title="My gap year in South America" rel="external">blogged about</a> at length.</p>
+			<p>I am <a href="http://www.wolpy.com/markormesher" title="Mark Ormesher's travel history" rel="external">keen to travel</a> within the UK and overseas, and my next &quot;big adventure&quot; will be a coast-to-coast tour of the United States, which I hope to complete before my final year of studies.</p>
 		</div>
 		<div class="column-fix"></div>
 	</div>
