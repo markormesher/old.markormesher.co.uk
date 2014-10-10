3387f12c3cc2cfde5185713f6d7914e358ec55e1
Mark Ormesher <me@markormesher.co.uk>
2014-10-10 01:33:33 +0100

    Home page title consistent with site

--- src/wp-content/themes/markormesher/functions.php
+++ src/wp-content/themes/markormesher/functions.php
@@ -4,7 +4,7 @@
 add_filter('wp_title', 'mo_wp_title_for_home');
 function mo_wp_title_for_home($title) {
 	if (empty($title) && (is_home() || is_front_page())) {
-		return get_bloginfo('title') . (strlen(get_bloginfo('description')) == 0 ? '' : ' | ' . get_bloginfo('description'));
+		return get_bloginfo('title') . (strlen(get_bloginfo('description')) == 0 ? '' : ' : ' . get_bloginfo('description'));
 	}
 	return $title;
 }
