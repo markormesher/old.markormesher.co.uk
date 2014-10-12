398fc7af92efa1f6b1ecf9c4d806263fc3fe6762
Mark Ormesher <mark@unitu.co.uk>
2014-10-12 13:54:13 +0100

    Updated about page

--- src/wp-content/themes/markormesher/about/index.php
+++ src/wp-content/themes/markormesher/about/index.php
@@ -2,11 +2,18 @@
 	<div class="container column-container">
 		<div class="five-half column">
 			<h1>About Me</h1>
-			<h2>Right Now: Work</h2>
+			<h2><i class="fa fa-wrench"></i> Now: Work</h2>
 			<p>As the <strong>lead Android developer</strong> for <a href="http://www.unitu.co.uk" title="Unitu: Learn Better, Together" rel="external">Unitu</a>, I am responsible for building and testing an app that will soon be deployed to 2500 UK university students.</p>
-			<h2>Programming</h2>
-			<p>I have been programming for 9 years, as a hobby, a trade and a passion. I have <a href="/skills" title="Mark Ormesher's programming skill">skills</a> in <strong>mobile, desktop and web</strong> technologies, including Android, Java, Python, PHP, JS/jQuery, (X)HTML and CSS.</p>
-			<h2>Teaching</h2>
+
+			<div class="hr"><hr/></div>
+
+			<h2><i class="fa fa-code"></i> Programming</h2>
+			<p>I have been programming for 9 years as a hobby, a trade and a passion. I have <a href="/skills" title="Mark Ormesher's programming skill">skills</a> in <strong>mobile</strong>, <strong>desktop</strong> and <strong>web</strong> technologies, including <strong>Android</strong>, <strong>Java</strong>, <strong>Python</strong>, <strong>PHP</strong>, <strong>JS/jQuery</strong>, <strong>(X)HTML</strong> and <strong>CSS</strong>.</p>
+			<p>In addition to these languages, I am eager to expand my skills and I'm always willing to learn something new. I've recently started working with Microsoft Azure servers, and I will soon be learning <strong>Ruby</strong> and <strong>Node.js</strong>.</p>
+
+			<div class="hr"><hr/></div>
+
+			<h2><i class="fa fa-graduation-cap"></i> Teaching</h2>
 			<p>As a <strong>teaching assistant</strong> at King's College, I provide support for the Advanced Practical Topics module, which gives students an insight to topics that they wouldn't otherwise cover.</p>
 			<p>In 2012/13, I also volunteered with the charity Project Trust as a <strong>secondary school teacher</strong> in South America. I taught maths, reading and PE to youths aged 13-16 in a remote hinterland village, and ran several extra-curricular projects.</p>
 			<p>My experiences overseas sparked a passion for education: since my return I have promoted Project Trust at careers fairs and involved myself in a number of educational projects, including a series of <strong>Android workshops</strong> that I organise and deliver through my work with the <a href="http://www.kcltech.com" title="KCL Tech Society" rel="external">KCL Tech Society</a>.</p>
@@ -19,11 +26,17 @@
 				<div>&nbsp;</div>
 			</div>
 			<p class="caption">Receiving the <strong>British Computing Society Award for Best Student</strong>. Left to right: me; Prof. Liz Bacon, BCS President; Prof. Peter McBurney, KCL Head of Informatics.</p>
-			<h2>Right Now: Studies</h2>
+			<h2><i class="fa fa-pencil"></i> Now: Studies</h2>
 			<p>I am a second-year Computer Science student at <strong>King's College London</strong>. I maintained a 93% average over my first year, and in 2015/16 I will complete a year-long industry placement.</p>
-			<h2>Awards</h2>
+
+			<div class="hr"><hr/></div>
+
+			<h2><i class="fa fa-trophy"></i> Awards</h2>
 			<p>In July 2014 I was awarded the <strong>British Computing Society Award for Best Student</strong>, as well as three other departmental awards from <strong>KCL</strong>, including best first-year results and an award for exceptional contribution.</p>
-			<h2>Travel</h2>
+
+			<div class="hr"><hr/></div>
+
+			<h2><i class="fa fa-globe"></i> Travel</h2>
 			<p>During my year of volunteering, I was lucky enough to travel to several South American and Caribbean countries, which I have <a href="http://www.marksprojecttrust.co.uk" title="My gap year in South America" rel="external">blogged about</a> at length.</p>
 			<p>I am <a href="http://www.wolpy.com/markormesher" title="Mark Ormesher's travel history" rel="external">keen to travel</a> within the UK and overseas, and my next &quot;big adventure&quot; will be a coast-to-coast tour of the United States, which I hope to complete before my final year of studies.</p>
 		</div>
