aaa455fbf8f6b8b4e836cec379775d187b93538c
Mark Ormesher <mark@unitu.co.uk>
2014-10-13 00:55:56 +0100

    Updates to go live (sync with server files)

--- src/wp-content/themes/markormesher/error/settings.php
+++ src/wp-content/themes/markormesher/error/settings.php
@@ -9,11 +9,4 @@ $includes[] = 'error/index.php';
 
 global $activeNavItem, $showHeader;
 $activeNavItem = '';
-switch (PAGE_ERROR) {
-	case 404:
-		$showHeader = '404: <span>Nothing Found</span>';
-		break;
-	default:
-		$showHeader = 'Error: <span>Unknown Cause</span>';
-		break;
-}
+$showHeader = '404: <span>Nothing Found</span>';
