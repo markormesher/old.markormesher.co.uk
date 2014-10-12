5d046a4cee2852591c99dcb2a76b960024e1833c
Mark Ormesher <mark@unitu.co.uk>
2014-10-12 21:36:09 +0100

    Basic input sanitisation

--- src/wp-content/themes/markormesher/index.php
+++ src/wp-content/themes/markormesher/index.php
@@ -1,4 +1,11 @@
 <?php
+// input sanitisation
+if (isset($_POST)) {
+	foreach ($_POST as $k => $v) {
+		$_POST[$k] = filter_var(trim($v), FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+	}
+}
+
 get_header();
 
 if (isset($includes) && is_array($includes)) {
