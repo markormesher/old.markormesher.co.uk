1eb0fc6373fb066aba91e2a0ba2c40ca03c56c93
Mark Ormesher <me@markormesher.co.uk>
2015-01-25 12:50:22 +0000

    Updated email sending

--- src/wp-content/themes/markormesher/contact/index.php
+++ src/wp-content/themes/markormesher/contact/index.php
@@ -3,18 +3,6 @@
 		<h1>Contact Me</h1>
 		<div class="eight column">
 			<?php
-
-			/*
-			 *
-			 * THIS IS TERRIBLY INSECURE, BECAUSE IT IS NOT FINISHED,
-			 * AND IT IS NOT PUBLICLY ACCESSIBLE.
-			 *
-			 * DO NOT USE THIS ON A PUBLIC SITE.
-			 *
-			 * HAPPY NOW, MUS?
-			 *
-			 */
-
 			if (isset($_POST['sent'])) {
 				$name = $_POST['user_name'];
 				$email = $_POST['email'];
@@ -33,8 +21,16 @@
 					$emailMsg .= "---------\n";
 					$emailMsg .= $message . "\n\n";
 					$emailMsg .= 'Sent on ' . date('j M Y \a\t H:i');
+					require('email-config.php'); // not included in git repo
 					require('/usr/share/php/Mail.php');
-					$factory =& Mail::factory('smtp',array('port'=>25,'auth'=>false));
+					$factory =& Mail::factory('smtp',
+						array(
+							'host' => $host,
+							'auth' => true,
+							'username' => $username,
+							'password' => $password
+						)
+					);
 					$headers['From'] = $name . '<' . $email . '>';
 					$headers['To'] = 'me@markormesher.co.uk';
 					$headers['Subject'] = 'From MarkOrmesher.co.uk';
@@ -55,23 +51,25 @@
 				}
 			}
 			?>
-			<p>If you'd like to get in touch with me, please feel free to <strong>send me an email</strong> with the form below, or use any of the other channels listed on this page. I'd love to hear from you, and I will get back to you as soon as possible - at most in a day or two.</p>
+			<p>If you'd like to get in touch with me, please feel free to
+				<strong>send me an email</strong> with the form below, or use any of the other channels listed on this page. I'd love to hear from you, and I will get back to you as soon as possible - at most in a day or two.
+			</p>
 			<form action="/contact" method="post">
 				<div class="form-field">
 					<label for="ff-name">Name</label>
-					<input type="text" name="user_name" id="ff-name" value="<?=(isset($_POST['user_name'])?$_POST['user_name']:'');?>"/>
+					<input type="text" name="user_name" id="ff-name" value="<?= (isset($_POST['user_name']) ? $_POST['user_name'] : ''); ?>"/>
 				</div>
 				<div class="form-field">
 					<label for="ff-email">Email Address</label>
-					<input type="email" name="email" id="ff-email" value="<?=(isset($_POST['email'])?$_POST['email']:'');?>"/>
+					<input type="email" name="email" id="ff-email" value="<?= (isset($_POST['email']) ? $_POST['email'] : ''); ?>"/>
 				</div>
 				<div class="form-field">
 					<label for="ff-phone">Phone Number (optional)</label>
-					<input type="tel" name="phone" id="ff-phone" value="<?=(isset($_POST['phone'])?$_POST['phone']:'');?>"/>
+					<input type="tel" name="phone" id="ff-phone" value="<?= (isset($_POST['phone']) ? $_POST['phone'] : ''); ?>"/>
 				</div>
 				<div class="form-field">
 					<label for="ff-message">Message</label>
-					<textarea name="message" id="ff-message"><?=(isset($_POST['message'])?$_POST['message']:'');?></textarea>
+					<textarea name="message" id="ff-message"><?= (isset($_POST['message']) ? $_POST['message'] : ''); ?></textarea>
 				</div>
 				<div class="form-button">
 					<input type="hidden" name="sent" value="1"/>
@@ -84,9 +82,13 @@
 		</div>
 		<div class="three column">
 			<p><i class="fa fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp;+44 (0) 7775 932063</p>
-			<p><i class="fa fa-envelope"></i>&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:mark@ormo.me" class="nonlink">mark@ormo.me</a></p>
+			<p>
+				<i class="fa fa-envelope"></i>&nbsp;&nbsp;&nbsp;&nbsp;<a href="mailto:mark@ormo.me" class="nonlink">mark@ormo.me</a>
+			</p>
 			<p><i class="fa fa-skype"></i>&nbsp;&nbsp;&nbsp;&nbsp;mark.ormo</p>
-			<p><i class="fa fa-twitter"></i>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.twitter.com/m_s_ormo" rel="external" class="nonlink">@m_s_ormo</a></p>
+			<p>
+				<i class="fa fa-twitter"></i>&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.twitter.com/m_s_ormo" rel="external" class="nonlink">@m_s_ormo</a>
+			</p>
 		</div>
 		<div class="column-fix"></div>
 	</div>
